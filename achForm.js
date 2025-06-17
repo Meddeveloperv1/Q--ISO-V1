9d31b1d606baadbe0b36bc20dfd5d94b9d1a7b2c
Tech Team <bbeattybrdi@gmail.com>
2025-06-17 09:12:59 -0700

    Create achForm.js

---
+++ achForm.js
@@ -0,0 +1,9 @@
+export default function ACHForm() {
+  return (
+    <div className="p-4">
+      <h2>Bank Transfer</h2>
+      <input placeholder="Routing #" className="border p-2" />
+      <input placeholder="Account #" className="border p-2" />
+    </div>
+  );
+}
