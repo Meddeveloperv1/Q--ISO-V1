243997e4e031c714667814306c77284dcce94926
Tech Team <bbeattybrdi@gmail.com>
2025-06-17 09:13:25 -0700

    Create biometric.js

---
+++ biometric.js
@@ -0,0 +1,7 @@
+export default function Biometric() {
+  return (
+    <div className="p-4">
+      <button className="bg-pink-700 text-white px-4 py-2 rounded">Use Biometric Login</button>
+    </div>
+  );
+}
